63937314cc2d65af95014d5a1f4f4b6af3bb67e3
sbland93 <rltmqj@gmail.com>
2016-08-21 21:55:30 +0900

    'h'

--- dist/server/config/environment/production.js
+++ dist/server/config/environment/production.js
@@ -14,5 +14,6 @@ module.exports = {
         mongo: {
                 uri: process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME || 'mongodb://localhost/constructive'
         }
+        
 };
 //# sourceMappingURL=production.js.map
