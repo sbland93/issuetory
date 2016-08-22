8b10bbdcac97ee395784e88abda49bd1a48e3ead
sbland93 <rltmqj@gmail.com>
2016-08-22 10:28:08 +0900

    'h'

--- dist/server/config/environment/production.js
+++ dist/server/config/environment/production.js
@@ -4,6 +4,7 @@
 // =================================
 
 module.exports = {
+
         // Server IP
         ip: process.env.OPENSHIFT_NODEJS_IP || process.env.IP || undefined,
 
@@ -12,7 +13,8 @@ module.exports = {
 
         // MongoDB connection options
         mongo: {
-                uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME || 'mongodb://localhost/constructive'
+                uri: process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME || 'mongodb://localhost/constructive'
         }
+
 };
 //# sourceMappingURL=production.js.map
